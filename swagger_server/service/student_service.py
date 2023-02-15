2ad90b5cac16fab11671dacfbd39acf800187de9
Ilja van Ipenburg <ivanipenburg@gmail.com>
2023-02-15 16:41:57 +0100

    fixed pymongo code

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -3,6 +3,7 @@ import tempfile
 from functools import reduce
 
 import pymongo
+from bson.objectid import ObjectId
 
 # from tinydb import Query, TinyDB
 
@@ -13,7 +14,7 @@ import pymongo
 
 # Create a local MongoDB database
 print(os.getenv('MONGO_URI'))
-myclient = pymongo.MongoClient(os.environ['MONGO_URI'])
+myclient = pymongo.MongoClient(os.environ.get('MONGO_URI', default='mongodb://localhost:27017'))
 mydb = myclient["mydatabase"]
 mycol = mydb["students"]
 
@@ -38,12 +39,13 @@ def add(student=None):
     # Check if student already exists
     myquery = {"first_name": student.first_name, "last_name": student.last_name}
     mydoc = mycol.find(myquery)
-    if mydoc:
+
+    if len(list(mydoc)) > 0:
         return 'already exists', 409
 
     mydict = student.to_dict()
     x = mycol.insert_one(mydict)
-    return student.student_id
+    return str(x.inserted_id)
 
 
 # def get_by_id(student_id=None, subject=None):
@@ -56,14 +58,16 @@ def add(student=None):
 
 
 def get_by_id(student_id=None, subject=None):
-    myquery = {"_id": student_id}
-    mydoc = mycol.find(myquery)
+    myquery = {"_id": ObjectId(str(student_id))}
+    mydoc = mycol.find_one(myquery)
 
     if not mydoc:
         return 'not found', 404
     
-    print(mydoc[0])
-    return mydoc[0]
+    # Remove the _id field from the dictionary
+    del mydoc['_id']
+
+    return mydoc
 
 
 # def delete(student_id=None):
@@ -74,8 +78,8 @@ def get_by_id(student_id=None, subject=None):
 #     return student_id
 
 def delete(student_id=None):
-    myquery = {"_id": student_id}
-    mydoc = mycol.find(myquery)
+    myquery = {"_id": ObjectId(str(student_id))}
+    mydoc = mycol.find_one(myquery)
 
     if not mydoc:
         return 'not found', 404
