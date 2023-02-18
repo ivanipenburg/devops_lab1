d3434558a3119b0bca51ef4f8bda38300b751ee8
Ilja van Ipenburg <ivanipenburg@gmail.com>
2023-02-18 16:56:13 +0100

    removed all code referring to tinydb

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -4,33 +4,12 @@ from functools import reduce
 
 # import pymongo
 from bson.objectid import ObjectId
-from tinydb import Query, TinyDB
-
-# db_dir_path = tempfile.gettempdir()
-# db_file_path = os.path.join(db_dir_path, "students.json")
-# student_db = TinyDB(db_file_path)
 
 # Create a local MongoDB database
 print(os.getenv('MONGO_URI'))
 myclient = pymongo.MongoClient(os.environ.get('MONGO_URI', default='mongodb://localhost:27017'))
 mydb = myclient["mydatabase"]
 mycol = mydb["students"]
-
-
-
-# def add(student=None):
-#     queries = []
-#     query = Query()
-#     queries.append(query.first_name == student.first_name)
-#     queries.append(query.last_name == student.last_name)
-#     query = reduce(lambda a, b: a & b, queries)
-#     res = student_db.search(query)
-#     if res:
-#         return 'already exists', 409
-
-#     student_db.insert(student.to_dict())
-    
-#     return student.student_id
 
 
 def add(student=None):
@@ -46,15 +25,6 @@ def add(student=None):
     return str(x.inserted_id)
 
 
-# def get_by_id(student_id=None, subject=None):
-#     student = student_db.get(doc_id=student_id)
-#     if not student:
-#         return 'not found', 404
-#     student['student_id'] = student_id
-#     print(student)
-#     return student
-
-
 def get_by_id(student_id=None, subject=None):
     myquery = {"_id": ObjectId(str(student_id))}
     mydoc = mycol.find_one(myquery)
@@ -67,13 +37,6 @@ def get_by_id(student_id=None, subject=None):
 
     return mydoc
 
-
-# def delete(student_id=None):
-#     student = student_db.get(doc_id=student_id)
-#     if not student:
-#         return 'not found', 404
-#     student_db.remove(doc_ids=[student_id])
-#     return str(student_id)
 
 def delete(student_id=None):
     myquery = {"_id": ObjectId(str(student_id))}
